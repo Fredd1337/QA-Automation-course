2413c1c7fde8c88a7c3cc52a33b1db0abdd94c9e
Fredd1337 <bogdansolodky@gmail.com>
2021-03-04 22:02:23 +0200

    My second commit

---
+++ src/hometask/com/CircleProgram.java
@@ -0,0 +1,114 @@
+package hometask.com;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+class FigureProgram {
+
+    public static void main(String[] args) {
+        Square sq = new Square();
+        Circle cl = new Circle();
+        Scanner sc = new Scanner(System.in);
+        double a = 0.5;
+        double b = 5;
+        double num1 = 0;
+        double num2 = 0;
+        double random_number1 = 0;
+        double random_number2 = 0;
+        int totalAttempts1 = 3;
+        int totalAttempts2 = 3;
+
+
+        do {
+            System.out.print("Введите длину стороны квадрата: ");
+            if (!sc.hasNextDouble()) {
+                System.out.println("Это не цифра!");
+                sc.next();
+                totalAttempts1--;
+                System.out.println("Осталось попыток: " + totalAttempts1);
+            }
+            else if (sc.hasNextDouble()) {
+                System.out.println("Правильно!");
+                num1 = sc.nextDouble();
+                totalAttempts1 = 0;
+            }
+        } while (totalAttempts1 > 0);
+
+        do {
+            if (num1 == 0) {
+                DecimalFormat df = new DecimalFormat("###.##");
+                System.out.println("Попытки закончились");
+                random_number1 = a + (Math.random() * b);
+                System.out.println("Случайная сторона квадрата: " + df.format(random_number1));
+            }
+        } while (random_number1 < 0);
+
+
+        do {
+            System.out.print("Введите радиус круга: ");
+            if (!sc.hasNextDouble()) {
+                System.out.println("Это не цифра!");
+                sc.next();
+                totalAttempts2--;
+                System.out.println("Осталось попыток: " + totalAttempts2);
+            }
+            else if (sc.hasNextDouble()) {
+                System.out.println("Правильно!");
+                num2 = sc.nextDouble();
+                totalAttempts2 = 0;
+            }
+        }  while (totalAttempts2 > 0);
+        do {
+            if (num2 == 0) {
+                DecimalFormat df = new DecimalFormat("###.##");
+                System.out.println("Попытки закончились");
+                random_number2 = a + (Math.random() * b);
+                System.out.println("Случайный радиус круга: " + df.format(random_number2));
+            }
+        } while (random_number2 < 0);
+
+
+        sq.areaCalculator(num1, random_number1);
+        cl.areaCalculator(num2, random_number2);
+
+        sq.DecimalFormat();
+        cl.DecimalFormat();
+
+    }
+}
+
+class Square {
+
+    double area1;
+
+    //Формула для нахождения площади квадрата
+    public double areaCalculator(double num1, double random_number1) {
+        if (num1 != 0)
+            area1 = num1 * num1;
+        else
+            area1 = random_number1 * random_number1;
+        return area1;
+    }
+    public void DecimalFormat(){
+        DecimalFormat df = new DecimalFormat("###.##");
+        System.out.println("Площадь квадрата: " + (df.format(area1)));
+    }
+}
+
+class Circle {
+
+    double area2;
+
+    //Формула для нахождения площади круга
+    public double areaCalculator(double num2, double random_number2) {
+        if  (num2 != 0)
+            area2 = Math.PI * (num2 * num2);
+        else
+            area2 = Math.PI * (random_number2 * random_number2);
+        return area2;
+    }
+     public void DecimalFormat(){
+         DecimalFormat df = new DecimalFormat("###.##");
+         System.out.println("Площадь круга: " + (df.format(area2)));
+     }
+}
