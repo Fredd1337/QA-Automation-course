f509a0800577089335f508a45702cb4ce720938c
Fredd1337 <bogdansolodky@gmail.com>
2021-03-15 21:04:51 +0200

    Commit for fourth task

---
+++ src/hometask4/ProgramTask4.java
@@ -0,0 +1,26 @@
+package hometask4;
+
+import java.util.Scanner;
+
+class ProgramTask4
+{
+    public static void main(String args[])
+    {
+        String original, reverse = "";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите строку на проверку: ");
+        original = sc.nextLine();
+
+        int length = original.length();
+
+        for ( int i = length - 1; i >= 0; i-- )
+            reverse = reverse + original.charAt(i);
+
+        if (original.equals(reverse))
+            System.out.println("Введённая строка палиндром.");
+        else
+            System.out.println("Введённая строка не палиндром.");
+
+    }
+}
